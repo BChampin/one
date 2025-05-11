0e3f403e194a08622b0cd64e73bc842c2b73e3d3
BChampin <github@champin.dev>
2025-05-11 17:38:30 +0200

    [chore] upgrading tailwind to v4

--- src/components/ui/command/CommandInput.vue
+++ src/components/ui/command/CommandInput.vue
@@ -33,7 +33,7 @@ const { filterState } = useCommand()
     <ListboxFilter
       v-bind="{ ...forwardedProps, ...$attrs }"
       v-model="filterState.search"
-      :class="cn('flex h-9 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50', props.class)"
+      :class="cn('flex h-9 w-full rounded-md bg-transparent py-3 text-sm outline-hidden placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50', props.class)"
     />
     <slot />
   </div>
