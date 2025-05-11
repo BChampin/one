cd3ad737ffe7fd4069e0789c1be3f3dd56e427e0
BChampin <github@champin.dev>
2025-05-11 18:59:41 +0200

    [chore] upgrading tailwind to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
 import path from 'node:path'
 import autoprefixer from 'autoprefixer'
-import tailwind from 'tailwindcss'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [tailwind(), autoprefixer()],
+      plugins: [autoprefixer()],
     },
   },
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
