2eec2bd7f1bc21e14c6183b3b7e0fda9d2974acf
BChampin <github@champin.dev>
2026-03-21 00:36:31 +0100

    chore(build): bootstrap vite workspace

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite-plus";
+import { defineConfig } from 'vite-plus'
 
 export default defineConfig({
   pack: {
@@ -8,4 +8,4 @@ export default defineConfig({
     exports: true,
     // ...config options
   },
-});
+})
